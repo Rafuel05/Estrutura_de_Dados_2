3c2b7b0950fbf208510e1999f4f7dc178c020af4
Rafuel05 <118372879+Rafuel05@users.noreply.github.com>
2023-10-20 19:27:46 +0000

    [Cntinuação Hash 20/10/2023

--- PictureHash.c
+++ PictureHash.c
@@ -3,12 +3,31 @@
 #include <string.h>
 #include "Hash.h"
 
-
+bool comparador (void* data1,void* data2)
+{
+    if(strcmp (data1,data2)==0) return true;
+    else return false;
+}
+void printar (void* data)
+{
+  printf("%s",*((char*)data));
+}
 void main ()
 {
     FILE * hash;
-    hash = fopen ("ListaDePalavrasPT.txt","w");
+    char *linha;
+    hash = fopen ("ListaDePalavrasPT.txt","r");
+    HashStruct tabela_hash;
+    initHash(&tabela_hash);
     if (hash){
-      
+        linha = (char*)malloc(sizeof(char) * 100);
+      while (fscanf(hash,"%s \n",linha ) !=EOF  ){
+       //printf ("%s \n",linha);
+    
+      put (&tabela_hash,linha,linha,comparador);
+      linha = (char*)malloc(sizeof(char) * 100);
     }
+    showHashStruct(&tabela_hash,printar);
+    fclose (hash);
+}
 }
