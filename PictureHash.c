ff53976918bceac32364e1a29c42b18203dc3415
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2023-11-09 15:36:00 +0000

    Trabalho Hash 3.1 Versão Final

--- PictureHash.c
+++ PictureHash.c
@@ -3,7 +3,40 @@
 #include <string.h>
 #include "Hash.h"
 #include "log.h"
+void createCollisionImage(const HashStruct *hashStruct, const char *filename) {
+    FILE *imageFile = fopen(filename, "w");
+    if (imageFile) {
+        int max_density = 0;
 
+        // Encontra a densidade máxima na tabela
+        for (int i = 0; i < MAX; i++) {
+            int density = hashStruct->hashes[i].size;
+            max_density = (density > max_density) ? density : max_density;
+        }
+
+        // Escreve o cabeçalho PPM no arquivo
+        fprintf(imageFile, "P3\n");
+        fprintf(imageFile, "32 32\n"); // Largura  x Altura 
+        fprintf(imageFile, "255\n");   // Valor máximo de cor
+
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 32; j++) {
+                int density = hashStruct->hashes[i * 32 + j].size;
+
+                // Calcule o valor de cor com base na densidade -> colisões
+                int color = (int)((density / (double)max_density) * 255);
+
+                // Escreve o valor de cor no arquivo PPM
+                fprintf(imageFile, "%d 0 100 ", color);
+            }
+        }
+
+        fclose(imageFile);
+        printf("Imagem PPM gerada com sucesso: %s\n", filename);
+    } else {
+        printf("Erro ao criar o arquivo de imagem PPM.\n");
+    }
+}
 bool comparador (void* data_1,void* data_2)
 {
     if(strcmp (data_1,data_2)==0) return true;
@@ -46,38 +79,4 @@ void main ()
     createCollisionImage(&tabela_hash, "PictureTableHash_1.ppm");
     createCollisionImage(&tabela_hash_2, "PictureTableHash_2.ppm");
     fclose (hash_2);
-}
-void createCollisionImage(const HashStruct *hashStruct, const char *filename) {
-    FILE *imageFile = fopen(filename, "w");
-    if (imageFile) {
-        int max_density = 0;
-
-        // Encontra a densidade máxima na tabela
-        for (int i = 0; i < MAX; i++) {
-            int density = hashStruct->hashes[i].size;
-            max_density = (density > max_density) ? density : max_density;
-        }
-
-        // Escreve o cabeçalho PPM no arquivo
-        fprintf(imageFile, "P3\n");
-        fprintf(imageFile, "32 32\n"); // Largura  x Altura 
-        fprintf(imageFile, "255\n");   // Valor máximo de cor
-
-        for (int i = 0; i < 32; i++) {
-            for (int j = 0; j < 32; j++) {
-                int density = hashStruct->hashes[i * 32 + j].size;
-
-                // Calcule o valor de cor com base na densidade -> colisões
-                int color = (int)((density / (double)max_density) * 255);
-
-                // Escreve o valor de cor no arquivo PPM
-                fprintf(imageFile, "%d 0 100 ", color);
-            }
-        }
-
-        fclose(imageFile);
-        printf("Imagem PPM gerada com sucesso: %s\n", filename);
-    } else {
-        printf("Erro ao criar o arquivo de imagem PPM.\n");
-    }
 }
