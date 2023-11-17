87ca035a8eff66c3a30ded1e785f342b63afe1a5
Rafael Melchior de Oliveira <118372879+Rafuel05@users.noreply.github.com>
2023-11-17 20:46:55 +0000

    Trabalho Arvore 1.0

--- aciiTableTree.c
+++ aciiTableTree.c
@@ -7,9 +7,75 @@ typedef struct Vetor {
     char caracter; 
 }Vetor;
 
+typedef struct Node {
+    int contador;
+    char caracter;
+    struct Node *esquerda;
+    struct Node *direita;
+} Node;
+
+Node *novoNo(int contador, char caracter) {
+    Node *novo = (Node *)malloc(sizeof(Node));
+    novo->contador = contador;
+    novo->caracter = caracter;
+    novo->esquerda = NULL;
+    novo->direita = NULL;
+    return novo;
+}
+
+void ordenarVetorDecrescente(Vetor vetor[], int tamanho) {
+    int i, j;
+    Vetor temp;
+
+    for (i = 1; i < tamanho; i++) {
+        temp = vetor[i];
+        j = i - 1;
+
+        while (j >= 0 && vetor[j].contador < temp.contador) {
+            vetor[j + 1] = vetor[j];
+            j = j - 1;
+        }
+        vetor[j + 1] = temp;
+    }
+}
+Node *construirArvoreHuffman(Vetor vetor[]) {
+    int tamanho = ASCII_SIZE;
+    ordenarVetorDecrescente(vetor, tamanho);
+
+    Node *raiz = NULL;
+    Node *esquerda, *direita, *novo;
+
+    while (tamanho > 1) {
+        esquerda = novoNo(vetor[tamanho - 1].contador, vetor[tamanho - 1].caracter);
+        direita = novoNo(vetor[tamanho - 2].contador, vetor[tamanho - 2].caracter);
+
+        novo = novoNo(esquerda->contador + direita->contador, '\0');
+        novo->esquerda = esquerda;
+        novo->direita = direita;
+
+        tamanho--;
+
+        vetor[tamanho - 1].contador = novo->contador;
+        vetor[tamanho - 1].caracter = novo->caracter;
+
+        ordenarVetorDecrescente(vetor, tamanho);
+    }
+
+    raiz = novoNo(vetor[0].contador, vetor[0].caracter);
+    return raiz;
+}
+
+void imprimirArvore(Node *raiz) {
+    if (raiz != NULL) {
+        printf("Caracter: %c | Contador: %d\n", raiz->caracter, raiz->contador);
+        imprimirArvore(raiz->esquerda);
+        imprimirArvore(raiz->direita);
+    }
+}
+
 int main() {
     FILE *arquivo;
-    char nome_arquivo[] = "teste.txt"; // Nome do arquivo a ser lido
+    char nome_arquivo[] = "arquivo.txt"; // Nome do arquivo a ser lido
     arquivo = fopen(nome_arquivo, "r");
 
     if (arquivo == NULL) {
@@ -27,12 +93,19 @@ int main() {
 
     fclose(arquivo);
 
-    // Exibindo o resultado
-    for (int i = 0; i < ASCII_SIZE; i++) {
+     ordenarVetorDecrescente(vetor, ASCII_SIZE);
+
+for (int i = 0; i < ASCII_SIZE; i++) {
         if (vetor[i].contador > 0) {
-            printf("Caractere: %c | Contador: %d\n", vetor[i].caracter, vetor[i].contador);
+            printf("Caractere: %c | Repetiçoes: %d\n", vetor[i].caracter, vetor[i].contador);
         }
     }
+     Node *raiz = construirArvoreHuffman(vetor);
+
+    printf("Árvore de Huffman:\n");
+    imprimirArvore(raiz);
+    // Exibindo o resultado
+    
 
     return EXIT_SUCCESS;
 }
